0cac7baa77829a50903693d9ff6bd987c1135e34
Matthew Jeshua Martin <matthew-martin@programmer.net>
2025-07-21 15:09:19 -0400

    Landing Page Complete, Book Menu Started

---
+++ webserver/src/main/java/com/evaitcsmatt/bookhub/webserver/controllers/BookController.java
@@ -0,0 +1,25 @@
+package com.evaitcsmatt.bookhub.webserver.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.evaitcsmatt.bookhub.webserver.service.BookService;
+import com.evaitcsmatt.bookhub.webserver.utils.BookServiceFactory;
+
+@Controller
+public class BookController {
+	
+	private final BookServiceFactory bookServiceFactory;
+	
+	public BookController(BookServiceFactory bookServiceFactory) {
+		this.bookServiceFactory = bookServiceFactory;
+	}
+	
+	@GetMapping(value = {"/books", "/books/"})
+	public String getBookMenu(Model model) {
+		model.addAttribute("listOfBooks", bookServiceFactory.getBookService(null).getAllBooks());
+		return "book-menu";
+	}
+	
+}
